1ec0083935bdde90dc53431f2afc2e97a289bd71
matin-jalali <matinjalalitekiyeh@gmail.com>
2024-06-24 16:57:55 -0400

    open close

---
+++ solid/open_close.hpp
@@ -0,0 +1,17 @@
+#ifndef OPEN_CLOSE_HPP
+#define OPEN_CLOSE_HPP
+
+#include <string>
+
+namespace solid::open_close {
+
+enum class Color { red, blue, green };
+enum class Size { small, medium, large };
+
+struct Product {
+
+};
+
+}
+
+#endif // OPEN_CLOSE_HPP
