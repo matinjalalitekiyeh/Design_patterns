34fb1ec6ddb3fcf2c8dc08243c8a00f39b727a3f
matin-jalali <matinjalalitekiyeh@gmail.com>
2024-06-25 10:14:21 -0400

    liskov

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "solid/single_responsibility.hpp"
 #include "solid/open_close.hpp"
+#include "solid/liskov.hpp"
 
 int main() {
 
     //solid
     //solid::single_responsibility::main();
-    solid::open_close::main();
+    //solid::open_close::main();
+    solid::liskov::main();
 
 
     return 0;
